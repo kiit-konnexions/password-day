36cd90766e275e6172e6395fcc423fe285da9289
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-03 22:45:07 +0530

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
   return (
     <div className="mt-16">
       <div className="flex items-center justify-center space-x-3">
-        <div className="h-[1px] w-32 bg-primary"></div>
+        <div className="h-[1px] w-28 bg-primary"></div>
         <span className="text-xs font-semibold text-primary whitespace-nowrap">
           FOLLOW US
         </span>
-        <div className="h-[1px] w-32 bg-primary"></div>
+        <div className="h-[1px] w-28 bg-primary"></div>
       </div>
       <div className="flex items-center justify-center text-xl space-x-4 mt-5 text-primary">
         <Link
